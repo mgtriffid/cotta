395f27b8bf93b5b9f479e63ed9d35278d5bf4c82
mgtriffid <mgtriffid@gmail.com>
2022-08-14 22:01:26 +0700

    Fix src dirs structure

--- desktop/src/main/java/com/mgtriffid/games/cotta/DesktopLauncher.java
+++ desktop/src/main/java/com/mgtriffid/games/cotta/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.mgtriffid.games.cotta;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mgtriffid.games.cotta.PannaGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {

9cae0e504bacc47e3b8b6e32cf305f4b9e553abd
mgtriffid <mgtriffid@gmail.com>
2022-08-14 21:56:45 +0700

    Initial commit

---
+++ desktop/src/com/mgtriffid/games/cotta/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mgtriffid.games.cotta;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mgtriffid.games.cotta.PannaGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("cotta");
+		new Lwjgl3Application(new PannaGame(), config);
+	}
+}
---
+++ server/src/com/mgtriffid/games/cotta/ServerLauncher.java
@@ -0,0 +1,7 @@
+package com.mgtriffid.games.cotta;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+        System.out.println("Hello Server");
+    }
+}

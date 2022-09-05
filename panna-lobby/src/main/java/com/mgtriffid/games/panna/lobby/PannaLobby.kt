11703c411c50af6abcdcfacd30dad27d34749798
mgtriffid <mgtriffid@gmail.com>
2022-09-05 12:01:13 +0700

    Introduce naivest "login" endpoint

--- panna-lobby/src/main/java/com/mgtriffid/games/panna/lobby/PannaLobby.kt
+++ panna-lobby/src/main/java/com/mgtriffid/games/panna/lobby/PannaLobby.kt
@@ -1,12 +1,18 @@
 package com.mgtriffid.games.panna.lobby
 
 import com.google.gson.Gson
+import spark.Request
+import spark.Response
+import spark.Spark
 import spark.Spark.get
 import spark.Spark.post
-import java.util.UUID
+import spark.kotlin.halt
+import java.util.*
+
 
 class PannaLobby {
     private val rooms: Rooms = Rooms()
+    private val sessions = HashMap<SessionToken, Username>()
 
     fun start() {
         initializeEndpoints()
@@ -22,7 +28,23 @@ class PannaLobby {
         // warms up data that is stored in the database)
         // list players in the room (why?)
         //
-
+        Spark.exception(
+            Exception::class.java
+        ) { exception: Exception, request: Request?, response: Response? -> exception.printStackTrace() }
+        post(
+            "/login",
+            { req, res ->
+                val (user, pass) = gson.fromJson(req.body(), LoginDto::class.java)
+                if (pass.startsWith(user)) {
+                    val token = UUID.randomUUID().toString()
+                    sessions[SessionToken(token)] = Username(user)
+                    SuccessfulLoginResponse(token)
+                } else {
+                    halt(401, "Login failed")
+                }
+            },
+            gson::toJson
+        )
         get(
             "/rooms",
             { _, _ -> RoomsDto(items = rooms.rooms.entries.map { RoomDto(it.key.roomId, it.value.map) }) },
@@ -62,3 +84,16 @@ data class CreateRoomDto(
 data class CreateRoomDtoResponse(
     val roomId: String
 )
+
+data class LoginDto(
+    val username: String,
+    val password: String
+)
+
+data class SessionToken(
+    val token: String
+)
+
+data class Username(val username: String)
+
+data class SuccessfulLoginResponse(val token: String)

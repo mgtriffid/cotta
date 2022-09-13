3b743c98a389ab29fd3f7b05b3b0d449b2bdf909
mgtriffid <mgtriffid@gmail.com>
2022-09-14 01:03:34 +0700

    Introduce screens

--- core/src/main/java/com/mgtriffid/games/panna/PannaGdxGame.java
+++ core/src/main/java/com/mgtriffid/games/panna/PannaGdxGame.java
@@ -1,25 +1,18 @@
 package com.mgtriffid.games.panna;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.mgtriffid.games.panna.screens.FirstScreen;
 
-public class PannaGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	PannaClient pannaClient;
-	
+public class PannaGdxGame extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		pannaClient = new PannaClient();
-		pannaClient.initialize();
+		setScreen(new FirstScreen(this));
 	}
 
 	@Override
 	public void render () {
+		super.render();
 		// called repeatedly so here we should call an adapter that could make client update when needed and render
 		// also when needed. But we don't know how responsibilities are shared in client code. Client is user interface
 		// plus state, not just state, so it's unfair to just decide all for client. Besides there is "predicted state"
@@ -28,16 +21,5 @@ public class PannaGdxGame extends ApplicationAdapter {
 		// which will first take care of real state, then of state plus predicted state, then finally of interpolated
 		// state that is useful for rendering. Something like that. Perhaps after we introduce some graphics it will be
 		// easier to develop something that would satisfy needs of most of LibGDX-oriented game developers.
-		pannaClient.update();
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }

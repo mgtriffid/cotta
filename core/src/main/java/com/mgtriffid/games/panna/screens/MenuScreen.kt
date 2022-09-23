c8d1374a0ab42544b422363c6984bee2f5f847e8
mgtriffid <mgtriffid@gmail.com>
2022-09-23 11:59:26 +0700

    Groom styles a bit

--- core/src/main/java/com/mgtriffid/games/panna/screens/MenuScreen.kt
+++ core/src/main/java/com/mgtriffid/games/panna/screens/MenuScreen.kt
@@ -8,12 +8,14 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.NinePatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.Action
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
 import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle
@@ -22,7 +24,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import com.mgtriffid.games.panna.PannaGdxGame
-import org.w3c.dom.Text
 
 class MenuScreen(
     private val game: PannaGdxGame
@@ -31,33 +32,62 @@ class MenuScreen(
     lateinit var stage: Stage
     lateinit var button: Button
     lateinit var backgroundTexture: Texture
+    lateinit var statusPanel: Table
+
+    object Styles {
+        val formInputLabelStyle = LabelStyle(BitmapFont(), Color.WHITE)
+        val textFieldStyle = TextFieldStyle(
+            BitmapFont(),
+            Color.YELLOW,
+            null,
+            null,
+            NinePatchDrawable(
+                NinePatch(
+                    Texture("textfield_9patch_atlas.png"),
+                    8, 8, 8, 8
+                )
+            )
+        )
+    }
 
     override fun show() {
         prepareStage()
         backgroundTexture = Texture("menu_bg.png")
     }
 
+    override fun render(delta: Float) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        stage.act(delta)
+        stage.batch.begin()
+        stage.batch.draw(backgroundTexture, 0f, 0f)
+        stage.batch.end()
+        stage.draw()
+    }
+
     private fun prepareStage() {
-        prepareTextFieldNinePatch()
         stage = Stage()
+
         Gdx.input.inputProcessor = stage
 
+        buildLoginForm()
+        buildStatusPanel()
+    }
+
+    private fun buildLoginForm() {
         val table = Table()
         table.setFillParent(true)
         table.debug = true
         stage.addActor(table)
-        val textFieldStyle = TextFieldStyle(
-            BitmapFont(),
-            Color.YELLOW,
-            null,
-            null,
-            prepareTextFieldNinePatch()
-        )
-        val loginInput = TextField("", textFieldStyle)
-        table.add(loginInput)
+        val loginInput = TextField("", Styles.textFieldStyle)
+        val loginLabel = Label("login", Styles.formInputLabelStyle)
+        table.add(loginLabel)
+        table.add(loginInput).width(300f).height(50f).pad(10f)
         table.row()
-        val passwordInput = TextField("", textFieldStyle)
-        table.add(passwordInput)
+        val passwordLabel = Label("password", Styles.formInputLabelStyle)
+        table.add(passwordLabel)
+        val passwordInput = TextField("", Styles.textFieldStyle)
+        passwordInput.isPasswordMode = true
+        table.add(passwordInput).width(300f).height(50f).pad(10f)
         table.row()
         val upTexture = Texture("blue_button_up.png")
         val downTexture = Texture("blue_button_down.png")
@@ -71,13 +101,14 @@ class MenuScreen(
         button.addListener(object : InputListener() {
             override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
                 Gdx.app.log("MenuScreen", "Button clicked")
-                "http://127.0.0.1:4567/login".httpPost().body("{\n  \"username\": \"${loginInput.text}\",\n  \"password\": \"${passwordInput.text}\"\n}\n")
-                .responseString { req, resp, result ->
-                    when (result) {
-                        is Result.Success -> println(result.get())
-                        is Result.Failure -> println(result.getException())
+                "http://127.0.0.1:4567/login".httpPost()
+                    .body("{\n  \"username\": \"${loginInput.text}\",\n  \"password\": \"${passwordInput.text}\"\n}\n")
+                    .responseString { req, resp, result ->
+                        when (result) {
+                            is Result.Success -> println(result.get())
+                            is Result.Failure -> println(result.getException())
+                        }
                     }
-                }
                 return true
             }
         })
@@ -86,17 +117,21 @@ class MenuScreen(
         table.add(button)
     }
 
-    override fun render(delta: Float) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        stage.act(delta)
-        stage.batch.begin()
-        stage.batch.draw(backgroundTexture, 0f, 0f)
-        stage.batch.end()
-        stage.draw()
-    }
-
-    private fun prepareTextFieldNinePatch(): NinePatchDrawable {
-        val ninePatch: NinePatch = NinePatch(Texture("textfield_9patch_atlas.png"), 8, 8, 8, 8)
-        return NinePatchDrawable(ninePatch)
+    private fun buildStatusPanel() {
+        statusPanel = Table()
+        statusPanel.setFillParent(true)
+        val statusPanelTextLabel = Label("neuzhto", Styles.formInputLabelStyle)
+        statusPanel.add(statusPanelTextLabel)
+        stage.addActor(statusPanel)
+        statusPanelTextLabel.addAction(object : Action() {
+            var acc = 0f
+            override fun act(delta: Float): Boolean {
+                acc += delta
+                println("acc = $acc")
+                statusPanelTextLabel.isVisible = (acc.toInt() % 2) == 1
+                println("statusPanelTextLabel.isVisible = ${statusPanelTextLabel.isVisible}")
+                return false
+            }
+        })
     }
 }

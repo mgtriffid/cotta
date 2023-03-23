9651b0363edbaeedbffdf5cbda16a99529ad29eb
mgtriffid <mgtriffid@gmail.com>
2023-03-23 13:26:43 +0700

    WIP Remove unused obsolete classes

--- cotta-core/src/main/java/com/mgtriffid/games/cotta/core/serialization/ServerToClientGameDataPiece.kt
+++
@@ -1,8 +0,0 @@
-package com.mgtriffid.games.cotta.core.serialization
-
-sealed class ServerToClientGameDataPiece {
-    abstract val tick: Long
-    // TODO no reason to have tick in DeltaPiece and Delta
-    data class DeltaPiece(override val tick: Long, val delta: Delta): ServerToClientGameDataPiece()
-    data class StatePiece(override val tick: Long, val stateSnapshot: StateSnapshot): ServerToClientGameDataPiece()
-}
--- cotta-server/src/main/kotlin/com/mgtriffid/games/cotta/server/impl/ClientGhost.kt
+++ cotta-server/src/main/kotlin/com/mgtriffid/games/cotta/server/impl/ClientGhost.kt
@@ -1,28 +1,14 @@
 package com.mgtriffid.games.cotta.server.impl
 
-import com.mgtriffid.games.cotta.core.entities.Entities
 import com.mgtriffid.games.cotta.network.ConnectionId
-import com.mgtriffid.games.cotta.core.serialization.Delta
-import com.mgtriffid.games.cotta.core.serialization.ServerToClientGameDataPiece
-import com.mgtriffid.games.cotta.core.serialization.StateSnapshot
-import com.mgtriffid.games.cotta.server.DataForClients
-import com.mgtriffid.games.cotta.utils.drain
 import java.util.*
-import java.util.concurrent.ConcurrentLinkedQueue
 
 private const val MAX_LAG_COMP_DEPTH_TICKS = 8
 
 // TODO inject history length
 class ClientGhost(val connectionId: ConnectionId) {
 
-    private var stateKnownToClient: Long? = null
-    private val queueToSend = ConcurrentLinkedQueue<ServerToClientGameDataPiece>()
     private val logOfSentData = TreeMap<Long, KindOfData>()
-
-    fun send(data: DataForClients, tick: Long) {
-        sendHistoricalDataIfNeeded(data, tick)
-        sendDelta(data, tick)
-    }
 
     // 20
     fun whatToSend(tick: Long): WhatToSend {
@@ -57,35 +43,6 @@ class ClientGhost(val connectionId: ConnectionId) {
         return logOfSentData.takeIf { it.isNotEmpty() }?.lastKey() ?: -1
     }
 
-    private fun sendHistoricalDataIfNeeded(data: DataForClients, tick: Long) {
-        sendState(data.entities(tick - 7), tick - 7)
-        for (t in (tick - 6) until tick) {
-            sendDelta(data, t)
-        }
-    }
-
-    private fun sendDelta(data: DataForClients, tick: Long) {
-        val curr = data.entities(tick).all()
-        val prev = data.entities(tick - 1).all()
-        // for these we only use ids
-        val removedEntitiesIds = prev.map { it.id }.toSet() - curr.map { it.id }.toSet()
-        // here we use ids and all components, or maybe a more efficient way to send, like if it's a blueprint then
-        // blueprint id will work fine
-        val addedEntities = curr.filter { c -> prev.none { p -> p.id == c.id } }
-        // and for these we calculate precisely components that were changed in at least one field
-        val changedEntities = curr.filter { c -> prev.any { p -> p.id == c.id } }
-        val delta = Delta(removedEntitiesIds, addedEntities, changedEntities, tick)
-        queueToSend.add(ServerToClientGameDataPiece.DeltaPiece(tick, delta))
-    }
-
-    private fun sendState(entities: Entities, tick: Long) {
-        queueToSend.add(
-            ServerToClientGameDataPiece.StatePiece(
-                tick = tick,
-                stateSnapshot = StateSnapshot(entities = entities.all().toSet())
-            )
-        )
-    }
 }
 
 interface WhatToSend {

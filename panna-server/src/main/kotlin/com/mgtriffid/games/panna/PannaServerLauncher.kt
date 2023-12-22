6f1972ac70a7508d1c2bb8acdc060c9fc8e1990d
mgtriffid <mgtriffid@gmail.com>
2023-12-22 16:27:57 +0700

    Rename java to folder to kotlin, just a small gradle build cleanup

--- panna-server/src/main/kotlin/com/mgtriffid/games/panna/PannaServerLauncher.kt
+++ panna-server/src/main/kotlin/com/mgtriffid/games/panna/PannaServerLauncher.kt
@@ -1,23 +1,19 @@
-package com.mgtriffid.games.panna;
+package com.mgtriffid.games.panna
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import com.mgtriffid.games.panna.lobby.PannaLobby;
-import org.slf4j.LoggerFactory;
+import com.mgtriffid.games.cotta.core.logging.configureLogging
+import com.mgtriffid.games.panna.lobby.PannaLobby
 
-import static com.mgtriffid.games.cotta.core.logging.LoggingConfigurationKt.configureLogging;
-
-public class PannaServerLauncher {
-    public static void main(String[] args) {
-        configureLogging();
-        new PannaLobby().start();
+object PannaServerLauncher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        configureLogging()
+        PannaLobby().start()
         // lobby can receive requests to join a room or start a room, so perhaps
         // we first instantiate a Server that runs somehow, then we make it possible
         // to call some methods of Server from Lobby. Like join, stop, create new, etc.
-
-        new Thread(() -> {
-            System.out.println("Starting server");
-            new PannaServer().start();
-        }).start();
+        Thread {
+            println("Starting server")
+            PannaServer().start()
+        }.start()
     }
 }

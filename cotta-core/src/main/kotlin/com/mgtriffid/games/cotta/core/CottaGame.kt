9d929fbd995be2c032284b91efabb30caf7ad677
mgtriffid <mgtriffid@gmail.com>
2023-12-29 14:15:10 +0700

    add a todo for usability

--- cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/CottaGame.kt
+++ cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/CottaGame.kt
@@ -10,14 +10,23 @@ import kotlin.reflect.KClass
 
 // Implement this to configure your actual game
 interface CottaGame {
+
     val serverSystems: List<KClass<*>>
+
     val nonPlayerInputProvider: NonPlayerInputProvider
     // how to start
     fun initializeServerState(entities: Entities)
+
+    // TODO make it not possible to add input components, ensure it only operates on static part, etc. More safety.
     fun initializeStaticState(entities: Entities)
+
     val componentClasses: Set<KClass<out Component<*>>>
+
     val inputComponentClasses: Set<KClass<out InputComponent<*>>>
+
     val effectClasses: Set<KClass<out CottaEffect>>
+
     val metaEntitiesInputComponents: Set<KClass<out InputComponent<*>>>
+
     val config: CottaConfig
 }

73ab4e12ca355e94503b563d89ca0499f5996e5c
mgtriffid <mgtriffid@gmail.com>
2024-01-22 22:45:45 +0700

    Remove obsolete classes

--- core/src/main/java/com/mgtriffid/games/panna/screens/game/graphics/DrawStrategy.kt
+++
@@ -1,7 +0,0 @@
-package com.mgtriffid.games.panna.screens.game.graphics
-
-import com.mgtriffid.games.cotta.core.entities.Entity
-
-interface DrawStrategy {
-    fun getTexture(e: Entity): TextureId
-}

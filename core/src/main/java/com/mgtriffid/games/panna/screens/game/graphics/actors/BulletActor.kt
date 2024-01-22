e3702d71f207f14e2f997c90eeca80b07293ebe8
mgtriffid <mgtriffid@gmail.com>
2024-01-22 21:16:30 +0700

    Draw bullets with respect to angle

--- core/src/main/java/com/mgtriffid/games/panna/screens/game/graphics/actors/BulletActor.kt
+++ core/src/main/java/com/mgtriffid/games/panna/screens/game/graphics/actors/BulletActor.kt
@@ -2,26 +2,33 @@ package com.mgtriffid.games.panna.screens.game.graphics.actors
 
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.MathUtils
 import com.mgtriffid.games.cotta.core.entities.Entity
-import com.mgtriffid.games.panna.flipped
 import com.mgtriffid.games.panna.shared.game.components.physics.VelocityComponent
 
 class BulletActor(private val textureRegion: TextureRegion) : PannaActor() {
-    private val leftVersion = textureRegion.flipped() // TODO don't create a new region for every new actor
-
     private var orientedRight = true
+    private var angle: Float = 0f
 
     override fun draw(batch: Batch, parentAlpha: Float) {
         super.draw(batch, parentAlpha)
         batch.draw(
-            if (orientedRight) textureRegion else leftVersion,
+            textureRegion,
             x - textureRegion.regionWidth / 2,
             y - textureRegion.regionHeight / 2,
+            textureRegion.regionWidth / 2f,
+            textureRegion.regionHeight / 2f,
+            textureRegion.regionWidth.toFloat(),
+            textureRegion.regionHeight.toFloat(),
+            1f,
+            1f,
+            angle
         )
     }
 
     override fun update(entity: Entity) {
         val velocityComponent = entity.getComponent(VelocityComponent::class)
+        angle = MathUtils.atan2Deg360(velocityComponent.velY, velocityComponent.velX)
         orientedRight = velocityComponent.velX > 0
     }
 }

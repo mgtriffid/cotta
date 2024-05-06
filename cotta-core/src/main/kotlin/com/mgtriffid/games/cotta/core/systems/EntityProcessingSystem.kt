5dd1fe6214a0554462d39dcc7800b7bd69d72cb6
mgtriffid <mgtriffid@gmail.com>
2024-05-06 23:24:54 +0700

    Add KDoc for EntityProcessingSystem

--- cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/systems/EntityProcessingSystem.kt
+++ cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/systems/EntityProcessingSystem.kt
@@ -3,6 +3,17 @@ package com.mgtriffid.games.cotta.core.systems
 import com.mgtriffid.games.cotta.core.entities.Entity
 import com.mgtriffid.games.cotta.core.simulation.invokers.context.EntityProcessingContext
 
+/**
+ * A system that processes entities. Each tick, each Entity is processed by each
+ * [EntityProcessingSystem].
+ */
 interface EntityProcessingSystem : CottaSystem {
+    /**
+     * Process the entity. Add or remove components, change their data here.
+     * Check the [Entity] for the components you need, if it doesn't have them,
+     * just `return`.
+     *
+     * This API is definitely a subject to change.
+     */
     fun process(e: Entity, ctx: EntityProcessingContext)
 }

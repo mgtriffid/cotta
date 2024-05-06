a7f35836585ea4f71cc34a7807765627227c5f60
mgtriffid <mgtriffid@gmail.com>
2024-05-07 00:30:05 +0700

    Some clarifying TODOs

--- cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/entities/Entities.kt
+++ cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/entities/Entities.kt
@@ -7,12 +7,12 @@ import com.mgtriffid.games.cotta.core.exceptions.EntityNotExistsException
 interface Entities {
     fun create(ownedBy: Entity.OwnedBy = Entity.OwnedBy.System): Entity
     fun get(id: EntityId): Entity?
-    // TODO make internal
+    // TODO make internal, should not be exposed to developers
     fun currentId(): Int
     fun getOrNotFound(id: EntityId): Entity = get(id) ?: throw EntityNotExistsException("Could not find entity $id")
     fun all(): Collection<Entity>
     fun dynamic(): Collection<Entity>
-    // GROOM ISP violated here, clearly
+    // TODO Should not be exposed to developers.
     fun create(id: EntityId, ownedBy: Entity.OwnedBy = Entity.OwnedBy.System): Entity
     fun remove(id: EntityId)
     // TODO should not be accessible for developers. Instead the whole static

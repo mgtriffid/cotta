4cb39c45e55daf92820e73ac5c0c0d76dfea14c8
mgtriffid <mgtriffid@gmail.com>
2024-09-17 12:36:13 +0500

    Add a memo comment

--- cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/annotations/Historical.kt
+++ cotta-core/src/main/kotlin/com/mgtriffid/games/cotta/core/annotations/Historical.kt
@@ -1,4 +1,6 @@
 package com.mgtriffid.games.cotta.core.annotations
 
+// BUG used only in component processor, should be used there and in
+//  ComponentRegistry to determine if a component is historical
 @Target(AnnotationTarget.CLASS)
 annotation class Historical
